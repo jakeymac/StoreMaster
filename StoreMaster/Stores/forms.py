a499c394f513bb303f24d087c2e19cc6de1073c6
Jacob Johnson <jakeymacaroni@gmail.com>
2023-10-15 02:38:05 -0600

    updates

--- StoreMaster/Stores/forms.py
+++ StoreMaster/Stores/forms.py
@@ -32,76 +32,25 @@ class StoreRegistrationForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         #Valid store information fields first
-        if self.store_name in Store.objects.values_list("store_name",flat=True):
+        if cleaned_data.get("store_name") in Store.objects.values_list("store_name",flat=True):
             #Retreive all stores that have this name to make sure there isn't one at this location already
-            stores = Store.objects.filter(store_name=self.store_name)
+            stores = Store.objects.filter(store_name=cleaned_data.get('store_name'))
             locations = [store.get_location() for store in stores]
-            if self.store_line_two:
-                if f"{self.store_address} {self.store_line_two} {self.store_zip} {self.store_city}, {self.store_state}" in locations:
+            if cleaned_data.get("store_line_two"):
+                if f"{cleaned_data.get('store_address')} {cleaned_data.get('store_line_two')} {cleaned_data.get('store_zip')} {cleaned_data.get('store_city')}, {cleaned_data.get('store_state')}" in locations:
                     raise forms.ValidationError("A store with name already exists at that address")
             else:
-                if f"{self.store_address} {self.store_zip} {self.store_city}, {self.store_state}" in locations:
+                if f"{cleaned_data.get('store_address')} {cleaned_data.get('store_zip')} {cleaned_data.get('store_city')}, {cleaned_data.get('store_state')}" in locations:
                     raise forms.ValidationError("A store with name already exists at that address")
                 
             if not self.manager:
-                if User.objects.filter(username=self.manager_username).exists():
+                if User.objects.filter(username=cleaned_data.get("manager_username")).exists():
                     raise forms.ValidationError("This username is already in use")
-                if User.objects.filter(email=self.manager_email_address).exists():
+                if User.objects.filter(email=cleaned_data.get("manager_email_address")).exists():
                     raise forms.ValidationError("This username is already in use")
 
 
             #raise ValidationError("A store with that name already exists")
 
         #validate manager information if it's been provided
-        existing_manager_val = cleaned_data.get("manager")
-        if not existing_manager_val:
-            new_manager_info = {"first_name":self.manager_first_name,
-                                "last_name":self.manager_last_name,
-                                "email_address":self.manager_email_address,
-                                "address":self.manager_address,
-                                "line_two":self.manager_line_two,
-                                "city":self.manager_city,
-                                "state":self.manager_state,
-                                "zip":self.manager_zip,
-                                "username":self.manager_username,
-                                "password":self.manager_password,
-                                "other_information":self.manager_other_information,
-                                "birthday":self.manager_birthday}
-            
-            new_user_form = UserRegistrationForm(initial=new_manager_info)
-
-            if new_user_form.is_valid():
-                #Establish new store
-                newStore = error
-
-
-                user = User(username=self.manager_username,
-                            password=self.manager_password,
-                            emai=self.manager_email_address)
-                
-                newManager = ManagerInfo(user=user,
-                                         first_name=self.manager_first_name,
-                                         last_name=self.manager_last_name,
-                                         email_address=self.manager_last_name,
-                                         address=self.manager_address,
-                                         line_two=self.manager_line_two,
-                                         city=self.manager_city,
-                                         state=self.manager_state,
-                                         zip=self.manager_zip,
-                                         username=self.manager_username,
-                                         password=self.manager_password,
-                                         other_information=self.manager_other_information,
-                                         birthday=self.manager_birthday,
-                                         store=newStore)
-                
-                newManager.save()
-
-                newStore = Store(store_name=self.store_name,
-                                 address=self.store_address,
-                                 line_two=self.store_line_two,
-                                 city=self.store_city,
-                                 state=self.store_state,
-                                 zip=self.store_zip)
-
-
         return cleaned_data
--- StoreMaster/Stores/views.py
+++ StoreMaster/Stores/views.py
@@ -11,8 +11,78 @@ def register_store(request):
     if request.method == "POST":
         #Check if the user has opted to use a pre-existing manager or register a new one
         newRegistrationForm = StoreRegistrationForm(request.POST)
-        if newRegistrationForm.is_valid():
-            return HttpResponse("Valid FORM Time")
+        errors = ""
+        try:
+            if newRegistrationForm.is_valid():
+                existing_manager_val = newRegistrationForm.cleaned_data["manager"]
+
+                newStore = Store(store_name=newRegistrationForm.cleaned_data["store_name"],
+                                        address=newRegistrationForm.cleaned_data["store_address"],
+                                        line_two=newRegistrationForm.cleaned_data["store_line_two"],
+                                        city=newRegistrationForm.cleaned_data["store_city"],
+                                        state=newRegistrationForm.cleaned_data["store_state"],
+                                        zip=newRegistrationForm.cleaned_data["store_zip"])
+                
+                #If a pre-existing manager was not chosen
+                if not existing_manager_val:
+                    new_manager_info = {"first_name":newRegistrationForm.cleaned_data["manager_first_name"],
+                                        "last_name":newRegistrationForm.cleaned_data["manager_last_name"],
+                                        "email_address":newRegistrationForm.cleaned_data["manager_email_address"],
+                                        "address":newRegistrationForm.cleaned_data["manager_address"],
+                                        "line_two":newRegistrationForm.cleaned_data["manager_line_two"],
+                                        "city":newRegistrationForm.cleaned_data["manager_city"],
+                                        "state":newRegistrationForm.cleaned_data["manager_state"],
+                                        "zip":newRegistrationForm.cleaned_data["manager_zip"],
+                                        "username":newRegistrationForm.cleaned_data["manager_username"],
+                                        "password":newRegistrationForm.cleaned_data["manager_password"],
+                                        "other_information":newRegistrationForm.cleaned_data["manager_other_information"],
+                                        "birthday":newRegistrationForm.cleaned_data["manager_birthday"]}
+                    
+                    new_user_form = UserRegistrationForm(initial=new_manager_info)
+
+                    if new_user_form.is_valid():
+                        newStore.save()
+                        
+                        user = User(username=newRegistrationForm.cleaned_data["manager_username"],
+                                    password=newRegistrationForm.cleaned_data["manager_password"],
+                                    emai=newRegistrationForm.cleaned_data["manager_email_address"])
+                        
+                        user.save()
+                        
+                        newManager = ManagerInfo(user=user,
+                                                first_name=newRegistrationForm.cleaned_data["manager_first_name"],
+                                                last_name=newRegistrationForm.cleaned_data["manager_last_name"],
+                                                email_address=newRegistrationForm.cleaned_data["manager_last_name"],
+                                                address=newRegistrationForm.cleaned_data["manager_address"],
+                                                line_two=newRegistrationForm.cleaned_data["manager_line_two"],
+                                                city=newRegistrationForm.cleaned_data["manager_city"],
+                                                state=newRegistrationForm.cleaned_data["manager_state"],
+                                                zip=newRegistrationForm.cleaned_data["manager_zip"],
+                                                username=newRegistrationForm.cleaned_data["manager_username"],
+                                                password=newRegistrationForm.cleaned_data["manager_password"],
+                                                other_information=newRegistrationForm.cleaned_data["manager_other_information"],
+                                                birthday=newRegistrationForm.cleaned_data["manager_birthday"],
+                                                store=newStore)
+        
+                        newManager.save()
+                    return HttpResponse("Valid FORM Time, created new objects")
+                else:
+                    newStore.save()
+                    manager = newRegistrationForm.cleaned_data["manager"]
+                    manager.store = newStore
+                    manager.save()            
+
+
+                    return HttpResponse("TESTINGGG")  
+            else:
+                return HttpResponse("ERROR:")
+        
+        
+        except ValidationError as e:
+            errors += f"{str(e)}\n"
+        
+        return HttpResponse(errors)
+                
         
     else:
         clean_form = StoreRegistrationForm()
