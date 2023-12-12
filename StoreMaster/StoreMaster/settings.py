8710a16ca63c4617f88ff691b146161a9c4b1606
Jacob Johnson <jakeymacaroni@gmail.com>
2023-12-12 16:21:07 -0700

    removed kolo

--- StoreMaster/StoreMaster/settings.py
+++ StoreMaster/StoreMaster/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "kolo.middleware.KoloMiddleware",
 ]
 
 ROOT_URLCONF = 'StoreMaster.urls'
