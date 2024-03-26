d18b38fa77a08b792669445e99bc2445f12a1e87
Jacob Johnson <jakeymacaroni@gmail.com>
2024-03-26 02:38:01 -0600

    added swagger for API documentation

--- StoreMaster/Stores/urls.py
+++ StoreMaster/Stores/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from . import views
 from . import endpoints
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API Documentation",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 app_name = "Stores"
 
@@ -34,4 +51,7 @@ urlpatterns = [
 
     path('api/store', endpoints.store_endpoint),
     path('api/store/<int:store_id>', endpoints.store_endpoint),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]   
